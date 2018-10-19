b3c315dfdd90b03e784de837d1c66c90b1698608
markc <markrc@hawaii.edu>
2018-10-18 21:12:52 -1000

    Searchbar and listview working together

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -3,19 +3,22 @@
         <ActionBar title="Home Page" class="action-bar"/>
         <!--<TabView height="100%" selectedTabColor="#53ba82" tabTextFontSize="15">-->
             <!--<TabViewItem title="Search Food" textTransform="uppercase">-->
-        <ScrollView>
-            <StackLayout>
+        <!--<ScrollView>-->
+        <!--<StackLayout>-->
+            <!---->
+        <!--</StackLayout>-->
+            <StackLayout orientation="vertical" width="100%" height="100%">
                 <SearchBar hint="red lentils" v-model="searchText" @submit="onSearchFoodTap"/>
-                <ListView class="list-group" for="item in items">
+                <RadListView ref="listView" for="item in items" @itemTap="onItemTap">
                     <v-template>
-                        <Label id="active task" :text="item.name" height="70"/>
-                        <!--<Label id="active task" :text="item.serving" height="70"/>-->
-                        <!--<Label id="active task" :text="item.photo" height="70"/>-->
-                        <!--<Label id="active task" :text="item.tagID" height="70"/>-->
+                        <StackLayout>
+                        <Label :text="item.name"></Label>
+                        <Label :text="item.serving"></Label>
+                        </StackLayout>
                     </v-template>
-                </ListView>
+                </RadListView>
             </StackLayout>
-        </ScrollView>
+        <!--</ScrollView>-->
             <!--</TabViewItem>-->
 
 
@@ -25,32 +28,21 @@
 
 <script>
     import axios from 'axios';
+
 export default {
     data() {
         return {
             items: [],
-            searchText: '',
+            searchText: 'butter',
             appID: "c32545ce",
             appKey: "d64606d7a3456c19baee2bd107dca747"
         };
     },
     methods: {
-
-        onButtonTap() {
-            const httpModule = require("http");
-            console.log("fuck");
-            let a = httpModule.getJSON("https://api.nal.usda.gov/ndb/V2/reports?ndbno=01009&ndbno=45202&ndbn" +
-                "o=35193&type=b&format=json&api_key=eXK8zYpri0nY8GW5nRgIkAYtUsEIydgamzUyDOvf&location=Honolulu+HI").then((r) => {
-                     //console.log(r);
-                     console.log("I'm here");
-                     this.extractJSON(r);
-            }, (e) => {
-                    console.log("Error?");
-                }
-            );
+        onItemTap( {item}) {
+          console.log(`Tapped on ${item}`);
         },
         onSearchFoodTap() {
-            const httpModule = require("http");
             axios.get("https://trackapi.nutritionix.com/v2/search/instant?query="+this.searchText, {
                 headers: {
                     "x-app-id": this.appID,
@@ -79,26 +71,11 @@ export default {
             }, error => {
                 console.error(error);
             });
-            // let request = httpModule.getJSON("https://trackapi.nutritionix.com/v2/search/instant?query="+
-            //     this.searchText+"?x-app-id=" + this.appID + "&x-app-key="+ this.appKey).then((r) => {
-            //         console.log("testing nutritionix api get request");
-            //         console.log(r);
-            // });
         },
-        onSearchNutrientTap() {
-            return null;
-        },
-        extractJSON(data) {
-            if(data.hasOwnProperty('foods')) {
-                console.log('hey hey seth here');
-                for(const entry of data.foods) {
-                    if(entry.hasOwnProperty('food')) {
-                        console.log(entry.food.desc.name);
-                        this.items.push(entry.food.desc.name);
-                    }
-                }
-            }
-        }
+    },
+    created() {
+        this.onSearchFoodTap();
+        console.log(this.items);
     }
 }
 </script>
--- app/main.js
+++ app/main.js
@@ -1,10 +1,12 @@
 import Vue from 'nativescript-vue';
-import App from './components/App'
-import Login from './components/Login'
-import Task from './components/Task'
-import Home from'./components/Home'
+import App from './components/App';
+import Login from './components/Login';
+import Task from './components/Task';
+import Home from'./components/Home';
+import RadListView from 'nativescript-ui-listview/vue';
 import VueDevtools from 'nativescript-vue-devtools'
 
+Vue.use(RadListView);
 Vue.registerElement('CardView', () => require('nativescript-cardview').CardView)
 if(TNS_ENV !== 'production') {
   Vue.use(VueDevtools)
